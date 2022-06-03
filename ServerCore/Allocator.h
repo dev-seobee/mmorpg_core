020a02a926404221010a1197b72d3aea566d9684
김명섭(Kim MyeongSeop) <seobee@ncsoft.com>
2022-06-03 17:00:50 +0900

    stomp allocator 구현 진행.

--- ServerCore/Allocator.h
+++ ServerCore/Allocator.h
@@ -10,3 +10,16 @@ public:
     static void     Release(void* ptr);
 };
 
+/*
+    StompAllocator
+    메모리 오염 버그를 잡는데 굉장히 유용.
+*/
+
+class StompAllocator
+{
+    enum { PAGE_SIZE = 0x1000 };
+
+public:
+    static void*    Alloc(int32 size);
+    static void     Release(void* ptr);
+};
