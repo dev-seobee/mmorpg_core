48cbef1df64b4ee0db2d8890a053e2c3ec2909e4
김명섭(Kim MyeongSeop) <seobee@ncsoft.com>
2022-06-16 19:54:32 +0900

    더미 클라이언트 접속

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,7 +1,47 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
-    
+    WSADATA wsaData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return 0;
+    }
+
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "SOCK Error : " << errCode << endl;
+        return 0;
+    }
+
+    SOCKADDR_IN serverAddr;
+    ::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    //serverAddr.sin_addr.S_un.S_addr = ::inet_addr("127.0.0.1");
+    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+    serverAddr.sin_port = ::htons(7777);
+
+    if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        int32 errCode = ::WSAGetLastError();
+        cout << "Connect Error << " << errCode << endl;
+        return 0;
+    }
+
+    while (true)
+    {
+        this_thread::sleep_for(1s);
+    }
+
+    ::closesocket(clientSocket);
+
+    ::WSACleanup();
 }
